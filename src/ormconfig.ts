8414ced92d824b595e223b09282007a7ed4d2929
Ryan Ward <ryan.ward@Ryans-MBP.frontiernet.net>
2021-08-10 02:20:00 -0500

    Migrations support

---
+++ src/ormconfig.ts
@@ -0,0 +1,47 @@
+import { AppConfig } from "./app.config";
+import { entities } from "./app.imports";
+
+const config = new AppConfig();
+
+let ormConfig;
+
+if (config.database.dbType === "sqlite") {
+  ormConfig = {
+    type: "sqlite",
+    database: config.database.sqlite.name,
+    entities,
+    migrations: ["dist/migrations/*.js"],
+    cli: {
+      migrationsDir: "src/migrations",
+    },
+  };
+} else if (config.database.dbType === "mysql") {
+  ormConfig = {
+    type: "mysql",
+    replication: {
+      master: {
+        host: config.database.mySql.host,
+        port: config.database.mySql.port,
+        username: config.database.mySql.userName,
+        password: config.database.mySql.password,
+        database: config.database.mySql.name,
+      },
+      slaves: [
+        {
+          host: config.database.mySql.replication.host,
+          port: config.database.mySql.replication.port,
+          username: config.database.mySql.userName,
+          password: config.database.mySql.password,
+          database: config.database.mySql.name,
+        },
+      ],
+    },
+    entities,
+    migrations: ["src/migrations/*.ts"],
+    cli: {
+      migrationsDir: "src/migrations",
+    },
+  };
+}
+
+module.exports = ormConfig;
